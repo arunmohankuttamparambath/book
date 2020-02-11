39cc48a91389c20ebe13749e1728fafd557a38c7
Arun Mohan Kuttamparambath <arkuttam@I-FLEX.COM>
2020-02-11 22:38:57 +0530

    Path Chnages to the service.py

--- service.py
+++ service.py
@@ -3,14 +3,14 @@ import sqlite3 as sql
 
 
 def register(fs_name,ls_name,su_name,email,username,password):
-    con = sql.connect("C:\Python_Workspace\Book\mydb\mybook.db")
+    con = sql.connect("D:\Python_Workspace\Book\mydb\mybook.db")
     cur = con.cursor()
     cur.execute("INSERT INTO t_users (f_name,l_name,s_name,email,u_name,p_word) VALUES (?,?,?,?,?,?)", (fs_name,ls_name,su_name,email,username,password))
     con.commit()
     con.close()
 
 def retrieveUsers(username,password):
-	con = sql.connect("C:\Python_Workspace\Book\mydb\mybook.db")
+	con = sql.connect("D:\Python_Workspace\Book\mydb\mybook.db")
 	cur = con.cursor()
 	cur.execute("SELECT * FROM t_users WHERE u_name = ? AND p_word = ?",(username,password))
 	users = cur.fetchone()
@@ -19,7 +19,7 @@ def retrieveUsers(username,password):
 
 	
 def retrieveId(uids):
-	con = sql.connect("C:\Python_Workspace\Book\mydb\mybook.db")
+	con = sql.connect("D:\Python_Workspace\Book\mydb\mybook.db")
 	cur = con.cursor()
 	cur.execute('SELECT * FROM t_users WHERE id = ?',(str(uids)))
 	users = cur.fetchone() 
